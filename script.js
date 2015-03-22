b3deafdd6858ac07e39636d89020b0839be17b56
Brianaamodt <brian.aamodt@gmail.com>
2015-03-22 16:45:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Dave (ex) {
-	console.log("This Is Dave. Dave has " + ex + " friends.")
+	console.log("This Is Dave. Dave has " + ex + " friends.");
 	ex+= (ex-2);
 	console.log("Dave posted several embaracing selfies, now dave only has " + ex +  " friends.\nLet's hope Dave learned a lesson");
 }
