f7d7075c16cba142fb7eab20843e4f1722a4c229
Brianaamodt <brian.aamodt@gmail.com>
2015-03-22 15:37:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function Dave(x){
-	var x = 10;
+	for (var x = 10; x >= 0; x--; ) {
 		if (x >= 3){
 			console.log("health is at " + x + " HP. Find a health pack");
-			x--;
 		} else if (x == 0){
 			console.log("health is at " + x + " HP. FATALITY!!!" );
-			x--;
 		} else {
 			console.log("health is at " + x + " HP.");
-			x--;
 		}
+	}
 }
