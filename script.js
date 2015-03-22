36ea512ed9eb89e083b8395bf679a2affa218fd4
Brianaamodt <brian.aamodt@gmail.com>
2015-03-22 16:04:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function Dave(x){
 	var x = prompt("lets do math! enter a number.");
 		if (x <= 50){
 			console.log(x + " - 5 = " + (x-5));
-		}
 		} else {
 			console.log(x + " + 36 = " + (x+36));
 	}
