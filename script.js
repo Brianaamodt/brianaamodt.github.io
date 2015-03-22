5a51f60a5b8d589d7c508bd66eb14f4f057863f2
Brianaamodt <brian.aamodt@gmail.com>
2015-03-22 14:39:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function Dave(x){
 	var x = str.lenth
 		if (x <= 8){
-			return("That's kinda short. Your full name really has only " + (x-1) + " letters?");}
+			return("That's kinda short. Your full name really has only " + (x-1) + " letters?");
 		} else {
 			return ("There are " + (x-1) + " letters in your name");
 }
