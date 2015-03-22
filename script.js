15204452ced4bf5c409b644895976ac17f934dfd
Brianaamodt <brian.aamodt@gmail.com>
2015-03-22 16:45:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function takeNum (num) {
-	console.log("I've take a variable into the function named \"num\" that is equal to "+num+" and is now being displayed")
-	num+=1;
-	console.log("I've now added 1 to the value and can display it here: "+num+" !");
+function Dave (ex) {
+	console.log("This Is Dave. Dave has " + ex + " friends.")
+	ex+= (ex-2);
+	console.log("Dave posted several embaracing selfies, now dave only has " + ex +  " friends.\nLet's hope Dave learned a lesson");
 }
